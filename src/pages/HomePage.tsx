7628d3596cdd5b23953faa785d40d3c00ed83a54
Evgenii <eugene.pokalyuk@gmail.com>
2024-06-13 15:07:10 +0700

    Update books data

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -70,22 +70,58 @@ const HomePage = () => {
         ]
     }];
 
-    const booksData = [{
-        title: "Александра Королькова",
-        books: [{ name: "Живая типографика", description: "В книге живым и доступным языком изложены основные правила и закономерности типографики—«искусства расположения наборного материала на плоскости листа»", link: "https://awdee.ru/kniga-zhivaya-tipografika-teper-besplatna/" }],
-    }, {
-        title: "Kamran Ahmed",
-        books: [{ name: "Frontend roadmap", description: "Frontend Developer. Step by step guide to becoming a modern frontend developer in 2024", link: "https://roadmap.sh/frontend" }],
-    }, {
-        title: "Вастрик",
-        books: [{ name: "👩‍🎤 Инди vs Корпорат 👨‍💼", description: "Почему стек и подход к пет-проектам диаметрально противоположен тому, что вы делаете на работе", link: "https://vas3k.blog/notes/indie_vs_corpo/" }],
-    }, {
-        title: "CSS Games",
-        books: [
-            { name: "Flexbox Froggy.", description: "Игра, в которой тебе нужно помочь лягушонку Фроги и его друзьям, написав CSS код", link: "https://flexboxfroggy.com/" },
-            { name: "Grid Garden", description: "Место, где вы напишете CSS-код, чтобы вырастить морковный сад", link: "https://cssgridgarden.com/" }
-        ],
-    }];
+    const booksData = [
+        {
+            title: "TypeHero",
+            books: [{
+                name: "Порешать задачки TS",
+                description: "Интерактивные задачки",
+                link: "https://typehero.dev/"
+            }],
+        }, {
+            title: "TS handbook",
+            books: [{
+                name: "Документация",
+                description: "манипуляции с типами, дженерики, type predicates.",
+                link: "https://www.typescriptlang.org/docs/handbook/"
+            }],
+        }, {
+            title: "LeetCode",
+            books: [{
+                name: "Порешать задачки JS",
+                description: "лучшая платформа, поможет вам апнуть свои скиллы, расширить знания и подготовиться к тех собесам)",
+                link: "https://leetcode.com/"
+            }],
+        }, {
+            title: "You Don't Know JS",
+            books: [{
+                name: "Серия книг",
+                description: "Погружение в основные механизмы языка JavaScript",
+                link: "https://github.com/azat-io/you-dont-know-js-ru"
+            }],
+        }, {
+            title: "Learn JS",
+            books: [{
+                name: "Учебник по JS",
+                description: "Перечитывай материалы, вникая глубже и выявляя ранее упущенные детали. Выполняй практические задания после каждого урока для закрепления знаний.",
+                link: "https://learnjs.ru/"
+            }],
+        }, {
+            title: "Александра Королькова",
+            books: [{ name: "Живая типографика", description: "В книге живым и доступным языком изложены основные правила и закономерности типографики—«искусства расположения наборного материала на плоскости листа»", link: "https://awdee.ru/kniga-zhivaya-tipografika-teper-besplatna/" }],
+        }, {
+            title: "Kamran Ahmed",
+            books: [{ name: "Frontend roadmap", description: "Frontend Developer. Step by step guide to becoming a modern frontend developer in 2024", link: "https://roadmap.sh/frontend" }],
+        }, {
+            title: "Вастрик",
+            books: [{ name: "👩‍🎤 Инди vs Корпорат 👨‍💼", description: "Почему стек и подход к пет-проектам диаметрально противоположен тому, что вы делаете на работе", link: "https://vas3k.blog/notes/indie_vs_corpo/" }],
+        }, {
+            title: "CSS Games",
+            books: [
+                { name: "Flexbox Froggy.", description: "Игра, в которой тебе нужно помочь лягушонку Фроги и его друзьям, написав CSS код", link: "https://flexboxfroggy.com/" },
+                { name: "Grid Garden", description: "Место, где вы напишете CSS-код, чтобы вырастить морковный сад", link: "https://cssgridgarden.com/" }
+            ],
+        }];
 
     const sceneData = [{
         name: "HTML5",
