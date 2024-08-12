654472c3ce42b13379add68015d518493a7e29c8
Evgenii <111701135+eugenepokalyuk@users.noreply.github.com>
2024-08-12 16:34:51 +0700

    Update App.tsx

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,15 @@
 import { Route, Routes, useLocation } from 'react-router-dom';
+
+import './App.css';
 import AboutPage from '../../pages/AboutPage';
 import ErrorPage from '../../pages/ErrorPage';
 import HomePage from '../../pages/HomePage';
 import ProjectsPage from '../../pages/ProjectsPage';
 import { ABOUT_PATH, DEFAULT_PATH, ERROR_PATH, PROJECTS_PATH } from '../../utils/routePath';
-import './App.css';
 
 const App = () => {
   const location = useLocation();
+  
   const background = location.state && location.state.background;
 
   return (
@@ -19,4 +21,5 @@ const App = () => {
     </Routes>
   )
 }
+
 export default App
