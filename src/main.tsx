5ed6fd872356294ec3f726f457a071d70f4ff517
Evgenii <111701135+eugenepokalyuk@users.noreply.github.com>
2024-08-12 16:36:52 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter as Router } from 'react-router-dom';
-import App from './components/App/App';
+
 import './index.css';
+import App from './components/App/App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
